2dd44a37f47af3fc30f05158fe38f6c38a983f9b
Karim Moumen <moumen.karim@gmail.com>
2020-02-24 00:47:07 -0500

    Remove commented code

--- app/models/publication_validator.rb
+++ app/models/publication_validator.rb
@@ -1,8 +1,6 @@
 class PublicationValidator < ActiveModel::Validator
 
   def validate(record)
-    #record.errors.add(:happened_at, 'published_at must be a valid datetime  ') if ((DateTime.parse(record.published_at)   rescue ArgumentError) == ArgumentError)
-    #record.errors.add(:happened_at, 'unpublished_at must be a valid datetime') if ((DateTime.parse(record.unpublished_at) rescue ArgumentError) == ArgumentError)
     if ((record.published_at.present? && record.unpublished_at.present?) && (record.published_at >= record.unpublished_at))
       return false
     else
